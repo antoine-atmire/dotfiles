18ba660b6968f353ca70644b89709b507db1555e
Antoine Snyers <antoine.snyers@skynet.be>
2016-09-08 19:01:36 +0200

    Add nvm and rvm to path.zsh

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -3,3 +3,7 @@ export PATH=$PATH:$HOME/bin
 if [ -f $HOME/.profile ]; then
   source $HOME/.profile
 fi
+export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
