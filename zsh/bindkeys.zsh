1dce1d65b6e845b6d144af9e0e90421ca8d7824d
Antoine Snyers <antoine.snyers@skynet.be>
2016-10-18 18:58:18 +0200

    Make ctrl-z also resume the suspended process

--- zsh/bindkeys.zsh
+++ zsh/bindkeys.zsh
@@ -1,4 +1,14 @@
 # esc-e to <c-x><c-e>
 bindkey "^[e" edit-command-line
-#bindkey "^[r" "source ~/.zshrc^J"
-bindkey -s '\er' 'source ~/.zshrc^J'                               # [Esc-l] - run command: ls
+bindkey "^[r" "source ~/.zshrc^J"
+
+# function to make ctrl-z suspend _and_ resume
+function fg-bg() {
+  if [[ $#BUFFER -eq 0 ]]; then
+    fg
+  else
+    zle push-input
+  fi
+}
+zle -N fg-bg
+bindkey '^z' fg-bg
