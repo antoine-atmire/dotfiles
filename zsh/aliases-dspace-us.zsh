0c994e840f1ac2a616beda78654c07e509e47d36
Antoine Snyers <antoine@atmire.com>
2018-03-26 10:34:14 +0200

    Add an alias to use dspsql with docker databases

--- zsh/aliases-dspace-us.zsh
+++ zsh/aliases-dspace-us.zsh
@@ -2,3 +2,6 @@ autoload -U +X bashcompinit && bashcompinit
 
 # ds-scripts by the us
 [ -f ~/.aliases ] && source ~/.aliases 
+
+alias -g D="2> /dev/null | docker exec -i $DSNAME psql -v ON_ERROR_STOP=1 -U dspace dspace6"
+
