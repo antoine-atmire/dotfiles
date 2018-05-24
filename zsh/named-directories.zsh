02604de9ab84a7d2c5bdce72a61f2ad310779890
Antoine Snyers <antoine@atmire.com>
2018-05-24 10:04:06 +0200

    Update zsh files

--- zsh/named-directories.zsh
+++ zsh/named-directories.zsh
@@ -1,6 +1,7 @@
 hash -d -- d=$HOME/Work/dspace
 hash -d -- m=$HOME/Work/modules
 hash -d -- m6=$HOME/Work/modules/modules-6.x/parent
+hash -d -- ma=$HOME/.m2/repository/com/atmire
 hash -d -- i=$HOME/IdeaProjects
 hash -d -- c=$HOME/Work/community
 hash -d -- j=$HOME/projects/judoforum
