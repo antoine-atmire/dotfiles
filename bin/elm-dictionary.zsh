14d558904b02856e91859290b034defa3ebb3188
Antoine Snyers <antoine@atmire.com>
2018-09-19 13:11:17 +0200

    Fixes to elm-dictionary.zsh

--- bin/elm-dictionary.zsh
+++ bin/elm-dictionary.zsh
@@ -1,8 +1,11 @@
 #!/usr/bin/env zsh
 
 setopt EXTENDED_GLOB
-grep "^\s*\w\+\s*=" ~/.elm/0.19.0/package/(^(tests|test|virtual-dom|Internal)/)#*.elm \
-    | sed -e 's/ //g' -e 's/=.*//' -e 's/\.elm:/\./' \
+grep "^\s*\w\+[^=]*=\s*$" ~/.elm/0.19.0/package/(^(tests|test|virtual-dom|Internal)/)#*.elm \
+    | grep -v "^\s--" \
+    | grep -v "\s*type\s" \
+    | sed -e 's/=.*//' -e 's/\.elm: */\./' \
+    | cut -d' ' -f 1 \
     | rev \
     | cut -d'/' -f 1 \
     | rev \
